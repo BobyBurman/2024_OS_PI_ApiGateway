90151056fb17ce33cff33b2d25e64e291b5d4612
Boby Burman <iamboby000@gmail.com>
2025-01-17 16:06:01 +0530

    apigateway with keyclock

---
+++ src/main/java/com/apigateway/routes/routes.java
@@ -0,0 +1,46 @@
+package com.apigateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.RequestPredicate;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+//Functional Endpoint Programming
+
+@Configuration
+public class routes {
+	
+	@Bean
+	public RouterFunction<ServerResponse> orderServiceRoute(){
+		
+		return GatewayRouterFunctions
+				.route("order-service")
+				.route(RequestPredicates.path("/api/order"),HandlerFunctions.http("http://localhost:8081"))
+				.build();
+	}
+	
+	@Bean
+	public RouterFunction<ServerResponse> productServiceRoute(){
+		
+		return GatewayRouterFunctions
+				.route("product-service")
+				.route(RequestPredicates.path("/api/product"),HandlerFunctions.http("http://localhost:8080"))
+				.route(RequestPredicates.path("/api/product/getAll"),HandlerFunctions.http("http://localhost:8080"))
+				.build();
+	}
+	
+	
+	@Bean 
+	public RouterFunction<ServerResponse> inventoryServiceRoute(){
+		
+		return GatewayRouterFunctions
+				.route("inventory-service")
+				.route(RequestPredicates.path("/api/inventory"),HandlerFunctions.http("http://localhost:8082"))
+				.build();
+	}
+}
