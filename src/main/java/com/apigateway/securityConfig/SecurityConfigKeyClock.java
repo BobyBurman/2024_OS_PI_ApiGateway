7d961b1ddb012d6bd8239cc99dedf7b7c8f502d9
Boby Burman <iamboby000@gmail.com>
2025-01-24 16:32:34 +0530

    api gate route config

--- src/main/java/com/apigateway/securityConfig/SecurityConfigKeyClock.java
+++ src/main/java/com/apigateway/securityConfig/SecurityConfigKeyClock.java
@@ -1,16 +1,30 @@
 package com.apigateway.securityConfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class SecurityConfigKeyClock {
 	
+	
+	//urls don't require authorization
+	private final String[] freeResourceUrls = {"/swagger-ui.html", "**/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.authorizeHttpRequests(authorize->authorize.anyRequest()
-		.authenticated())
-		.oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
-		.build();
+		return httpSecurity.authorizeHttpRequests(
+				authorize -> authorize
+				.requestMatchers(freeResourceUrls)
+				.permitAll()
+				.anyRequest()
+				.authenticated())
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
 	}
-
+	
+	
 }
